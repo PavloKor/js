2b7e1a4dacabccc7fde4fc61623677c215557980
PavloKor <110405849+PavloKor@users.noreply.github.com>
2023-06-02 02:22:33 +0300

    2 lesson

---
+++ types-2-less.js
@@ -0,0 +1,65 @@
+//                           Data types
+// Primityve (Примітиви)
+let greetings = 'welcome';
+// alert(greetings);
+console.log(typeof greetings);// - string
+greetings = 10;
+console.log(typeof greetings);// - number
+
+// Boolean
+const isBoolTrue = true;
+const isBoolFalse = false;
+console.log(typeof isBoolTrue);
+console.log(typeof isBoolFalse);
+
+// Null
+const someNull = null;// - нічого
+console.log(someNull);
+
+
+// Undefined (невизначений, змінна щє не отримала свого значення)
+// const someUndfnd = undefined; // невизначене значення
+let someUndfnd;
+console.log(someUndfnd);
+
+
+//                        Objects (Об'єкти)
+
+const obj = {};
+// const obj2 = new Object();  Старий спосіб. Використовуємо новий коротший
+console.log(obj);
+// console.log(obj2);
+const user = {
+  name: 'Pavlo',
+  age: 27,
+  gender: 'male'
+};
+console.log(user);
+
+// можна додавати до об'єкту властивості інакше
+const user2 = {};
+user2.name = 'Bob';
+user2.age = 23;
+// user2['gender'] = 'male'; // Старий варіант, використовуємо коротший і легший
+user2.gender = 'male';
+console.log(user2);
+
+// Big Int
+const bigInt = 99999999999999999999999999999999999999999n;
+const bigInt2 = BigInt('99999999999999999999999999999999999999999');
+console.log(99999999999999999999999999999999999999999)
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(bigInt);
+console.log(bigInt2);
+
+// Symbol
+
+const userN = {
+  id: 10,
+  age: 20,
+  height: 190,
+}
+const id = Symbol('some new symbol');
+userN[id] = 100;
+console.log(userN[id]);
+console.log(userN);
---
+++ variable-1st-les.js
@@ -0,0 +1,29 @@
+// Variables 
+// var ( вже не потрібен ), 
+// let, const - основні
+// let можна змінювати, const не можна ( константа )
+
+/*
+let name = 'pavlo';
+console.log(name);
+name = 'bob';
+console.log(name);
+const age = 20;
+console.log(age);
+// age = 30;
+// console.log(age); НЕ ПРАЦЮЄ!!!
+*/
+
+/*
+// Приклад використання змінноі для віттання користувачів на сайті. 
+// Можна змінити в будь який момент для всіх разом.
+
+
+let greetingsForUsers = 'Hello my user!';
+alert(greetingsForUsers);
+*/
+// Приклад для сайту
+/*
+let greetings = 'Welcome to club';
+alert(greetings);
+*/
