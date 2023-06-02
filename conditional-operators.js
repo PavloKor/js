d683e4bc7e8a5cdce0c7105f9fb942ac9f8be070
PavloKor <110405849+PavloKor@users.noreply.github.com>
2023-06-02 16:34:24 +0300

    new lessons

---
+++ conditional-operators.js
@@ -0,0 +1,60 @@
+// conditional operators
+
+// if/else operators
+
+// let isUserLoggedIn = true;
+// isUserLoggedIn = false;
+
+// if (isUserLoggedIn) {// 
+//   alert('u`re welcome')
+// } else {                                    
+//   alert('sorry, bro!')
+// }
+
+// Tearnary operator
+
+/*
+let isUserLoggedIn = true;
+isUserLoggedIn = false;
+
+isUserLoggedIn ? alert('u`re welcome') : alert('sorry, bro!'); // це if/else тернарно
+*/
+
+
+// Switch operator
+
+const age = 30;
+
+
+/*
+switch (age) {
+  case 10:
+    alert('Happy b-day, give me a kiss!')
+    break;
+  case 20:
+    alert('Happy b-day, give me a hug!')
+    break;
+  case 30:
+    alert('Happy b-day, give me a grandchildren!')
+    break;
+  default:
+    alert('Happy b-day, give me youre legacy!')
+  
+}
+*/
+const name = 'Pavlo'; 
+
+switch (name) {
+  case "Pavlo":
+    alert('Welcome to the club')
+    break;
+  case 'Denis':
+    alert('Ok, welcome')
+    break;
+  case 'Artem':
+    alert('Welcome')
+    break;
+  default:
+    alert('Good luck bitch!')
+  
+}
---
+++ operators-2part.js
@@ -0,0 +1,65 @@
+
+
+// arefmetic
+const num1 = 10;
+const num2 = 5;
+let num3 = 50;
+num3 += 30; // num3 = num3 + 30; - скорочення
+/*
+console.log(num3)
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2); // остача
+*/
+
+
+const age = 18;
+
+/*
+if (age === 18) {
+  alert ('youre welcome');
+}
+*/
+/*
+if (age >= 18) {
+  alert ('youre welcome');
+}
+*/
+/*
+if (age !== 18) {
+  alert ('youre welcome');
+}
+*/
+
+let name = "Pavlo";
+
+/*
+if (name === "Pavlo") {
+  alert ('Hello my friend!')
+}
+*/
+
+
+// Logic operator
+
+// Оператоv  р " і "
+/*
+if (age === 18 && name === 'Pavlo') { // якщо  провалиться хоч одна з умов перевірка не спрацює - це і
+  alert('Congrats'); // !== ===           
+}
+*/
+// Оператор " або "
+/*
+if (age === 18 || name === 'Pavlo') { // якщо  пройде хоч одна з умов перевірка спрацює - це або
+  alert('Congrats'); // !== ===           
+}
+*/
+
+// String operators
+
+name += ' is the best';
+console.log(name);
+
+console.log('my ' + 'string'); // відступ прописуємо у лапках якщо треба
