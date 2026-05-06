bf906760ba053b97b78f19d69e76df8ca409b163
PavloKor <110405849+PavloKor@users.noreply.github.com>
2026-05-06 19:49:21 +0300

    Update module-2.js

--- module-2.js
+++ module-2.js
@@ -178,6 +178,168 @@ if (age >= 18) {
 }
 
 console.log(type); // 'adult'
+
+///////////
+
+function getPrice(subscription) {
+  let price = 0;
+	
+  if (subscription === "pro") {
+	price = 100;
+  }
+	
+  return price;
+}
+
+console.log(getPrice("free")); // 0	
+console.log(getPrice("pro")); // 100	
+
+
+function checkGrade(grade) {
+  if (grade >= 70) {
+    return "Satisfactory";
+  } else {
+    return "Unsatisfactory";
+  }
+}
+
+console.log(checkGrade(40)); // Unsatisfactory
+console.log(checkGrade(75)); // Satisfactory
+
+function checkStorage(available, ordered) {
+  if (ordered == 0) {
+    return "There are no products in the order!";
+  } else if (ordered > available) {
+    return "Your order is too large, there are not enough items in stock!";
+  } else {
+    return "The order is accepted, our manager will contact you";
+  }
+}
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(70, 0));
+console.log(checkStorage(200, 20));
+console.log(checkStorage(200, 0));
+console.log(checkStorage(150, 0));
+console.log(checkStorage(80, 80));
+
+////// Тернарний оператор
+// Тернарний оператор — це коротша синтаксична заміна інструкції if...else. 
+// Тернарний оператор дозволяє виконувати певну частину коду залежно від умови.
+
+function getBiggerNumber(a, b) {
+  return a > b ? a : b;
+}
+
+console.log(getBiggerNumber(5, 10)); // 10
+console.log(getBiggerNumber(20, 15)); // 20
+console.log(getBiggerNumber(7, 7)); // 7
+
+
+function checkPassword(password) {
+  const correctPassword = "jqueryismyjam";
+  return password == correctPassword ? "Access granted" : "Access denied, wrong password!";
+}
+console.log(checkPassword("jqueryismyjam"));
+console.log(checkPassword("angul4r1sl1f3"));
+console.log(checkPassword("r3actsux"));
+
+///////// Оператор switch
+
+
+
+// Оператор switch дозволяє виконувати різні дії залежно від значення виразу. 
+// Використання switch є більш компактним і зручним способом для порівняння виразів з кількома варіантами, ніж інструкції if...else та else...if.
+
+//Ось загальний синтаксис оператора switch:
+
+/*
+switch (expression) {
+  case value1:
+    // код, що виконується, якщо вираз (expression) дорівнює value1
+    break;
+  case value2:
+    // код, що виконується, якщо вираз (expression) дорівнює value2
+    break;
+  // ...
+  default:
+    // код, що виконується, якщо вираз (expression) не відповідає жодному значенню
+}
+
+
+Як це працює:
+
+
+
+Вираз (expression) в операторі switch обчислюється.
+Значення виразу порівнюється з кожним блоком case зверху вниз.
+Якщо значення виразу відповідає значенню в блоці case, виконується код цього блоку.
+Коли виконання коду блоку case завершено, необхідно використовувати оператор break, щоб вийти зі switch. Це запобігає виконанню коду в наступних блоках case.
+Якщо жодне значення не відповідає виразу, виконається код у блоці default (якщо він є).
+
+*/
+
+///// Приклад використання оператора switch:
+
+/*
+const fruit = 'apple';
+
+switch (fruit) {
+  case 'orange':
+    console.log('Orange selected');
+    break;
+  case 'banana':
+    console.log('Banana selected');
+    break;
+  case 'apple':
+    console.log('Apple selected');
+    break;
+  default:
+    console.log('The fruit is unknown');
+}
+*/
+/// Оператор break
+//Після виконання коду в одному з випадків потрібно використовувати оператор break, щоб вийти з оператора switch.
+//Якщо break не вказано, виконання коду продовжиться в наступному case і далі. Така поведінка називається "провалюванням" (fall-through). 
+//Якщо потрібно, щоб кілька блоків case виконували той самий код, можна опустити оператор break між ними.
+
+/*
+const fruit = 'apple';
+
+switch (fruit) {
+  case 'apple':
+    console.log('Apple selected');
+  case 'banana':
+    console.log('Banana selected');
+    break; ///// це буде останнім виконаним рядком, тому що після нього стоїть break    
+  case 'orange':
+    console.log('Orange selected');
+    break;
+  default:
+    console.log('The fruit is unknown');
+/////////////////////////////
+    function getSubscriptionPrice(type) {
+    switch (type) {
+        case (`starter`):
+            return 0;
+        break;
+        case (`professional`):
+            return 20;
+        break;
+        case (`organization`):
+            return 50;
+        break;
+        default:
+            return `Invalid subscription type!`;
+    }
+}
+
+console.log(getSubscriptionPrice("professional"));
+console.log(getSubscriptionPrice("organization"));
+console.log(getSubscriptionPrice("starter"));
+console.log(getSubscriptionPrice("random"));
+console.log(getSubscriptionPrice("premium"));
+}
 */
 
 
